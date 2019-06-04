ef30f5ec7b93ff6a7bcf1921f983db3b5ebbe478
Franklin <FLIU253@uwo.ca>
2019-06-03 22:55:16 -0400

    changes

--- src/Experience.js
+++ src/Experience.js
@@ -1,79 +1,8 @@
-import React from "react";
-import "./Experience.css";
+import React, { Component } from "react";
 
-const Experience = () => {
-  return (
-    <div className="experience" id="experience">
-      <h1 className="text-center p-4">EXPERIENCE</h1>
-      <div className="container pb-4">
-        <div className="card mb-5">
-          <h5 className="card-header">Autodata Solutions</h5>
-          <div className="card-body">
-            <h5 className="card-title">QA Automation Analyst Intern</h5>
-            <ul className="card-text">
-              <li>To Be Determined</li>
-            </ul>
-            <a
-              href="http://www.autodata.net/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="btn btn-primary"
-            >
-              Comepany Website
-            </a>
-            <p className="card-text pt-3">
-              <small className="text-muted">
-                May 2019 - September 2020 | London, Ontario
-              </small>
-            </p>
-          </div>
-        </div>
-
-        <div className="card mb-5">
-          <h5 className="card-header">Western University</h5>
-          <div className="card-body">
-            <h5 className="card-title">
-              Instructional Technology Resource Centre Consultant
-            </h5>
-            <ul className="card-text">
-              <li>
-                Assisted professors and faculty members by explaining and
-                teaching them how to use different technologies, including
-                Western's learning management system
-              </li>
-              <li>
-                Created and edited CSS and HTML files for faculty members'
-                course websites to enhance the students' learning experience
-              </li>
-              <li>
-                Collaborated with colleagues and facult members to produce
-                instructional videos using Adboe Photoshop and Adobe Premiere
-                Pro
-              </li>
-              <li>
-                Performed administrative duties, including managing incoming
-                calls, booking appointments, and directing clients to the
-                appropriate technical staff for assistance
-              </li>
-            </ul>
-            <a
-              href="https://itrc.uwo.ca/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="btn btn-primary"
-            >
-              Company Website
-            </a>
-            <p className="card-text pt-3">
-              <small className="text-muted">
-                May 2018 - April 2019 | London, Ontario
-              </small>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
+class Experience extends Component {
+  render() {
+    return <div>testing</div>;
+  }
+}
 export default Experience;
--- src/Skill.js
+++ src/Skill.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Skill.css";
 import CSS from "./images/CSS.png";
 import HTML5 from "./images/HTML5.png";
@@ -13,120 +13,175 @@ import javascript from "./images/javascript.png";
 import python from "./images/python.png";
 import cplus from "./images/c++.png";
 
-const Skill = () => {
-  return (
-    <div className="skill" id="skill">
-      <h1 className="text-center p-4">SKILLS</h1>
+class Skill extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      developmentCollapse: false,
+      peopleSkillCollapse: false,
+      uiUxCollapse: false
+    };
+  }
 
-      <div className="container pb-5">
-        <div className="card mb-3">
-          <div className="card-header">
-            <h3>Frameworks</h3>
-          </div>
-          <div className="card-body">
-            <blockquote className="blockquote mb-0">
-              <div className="container">
-                <div className="row">
-                  <div className="col">
-                    <img
-                      className="skillPic"
-                      src={BLOGO}
-                      alt="bootstrap logo"
-                    />
-                    <div className="col">BOOTSTRAP</div>
-                  </div>
-                  <div className="col">
-                    <img className="skillPic" src={RLogo} alt="react logo" />
-                    <div className="col">REACT</div>
-                  </div>
-                  <div className="col">
-                    <img
-                      className="skillPic"
-                      src={angular}
-                      alt="angular logo"
-                    />
-                    <div className="col">ANGULAR</div>
-                  </div>
-                  <div className="col">
-                    <img className="skillPic" src={node} alt="node logo" />
-                    <div className="col">Node.Js</div>
-                  </div>
-                </div>
-              </div>
-            </blockquote>
-          </div>
-        </div>
+  developmentCollapsePressed = () => {
+    this.setState({
+      developmentCollapse: !this.state.developmentCollapse
+    });
+  };
 
-        <div className="card">
-          <div className="card-header">
-            <h3>Languages</h3>
-          </div>
-          <div className="card-body">
-            <blockquote className="blockquote mb-0">
-              <div className="container">
-                <div className="row">
-                  <div className="col">
-                    <img className="skillPic" src={java} alt="java logo" />
-                    <div className="col">JAVA</div>
-                  </div>
-                  <div className="col">
-                    <img
-                      className="skillPic"
-                      src={javascript}
-                      alt="javascript logo"
-                    />
-                    <div className="col">JAVASCRIPT</div>
-                  </div>
-                  <div className="col">
-                    <img className="skillPic" src={python} alt="python logo" />
-                    <div className="col">PYTHON</div>
-                  </div>
-                  <div className="col">
-                    <img className="skillPic" src={cplus} alt="c++ logo" />
-                    <div className="col">C++</div>
-                  </div>
-                  <div className="col">
-                    <img className="skillPic" src={CSS} alt="Bootstrap logo" />
-                    <div className="col">CSS</div>
-                  </div>
-                  <div className="col">
-                    <img className="skillPic" src={HTML5} alt="HTML5 logo" />
-                    <div className="col">HTML5</div>
-                  </div>
-                </div>
-              </div>
-            </blockquote>
-          </div>
-        </div>
+  peopleSkillCollapsePressed = () => {
+    this.setState({
+      peopleSkillCollapse: !this.state.peopleSkillCollapse
+    });
+  };
 
-        <div className="card">
-          <div className="card-header">
-            <h3>Backend</h3>
-          </div>
-          <div className="card-body">
-            <blockquote className="blockquote mb-0">
-              <div className="container">
-                <div className="row">
-                  <div className="col">
-                    <img className="skillPic" src={mysql} alt="mysql logo" />
-                    <div className="col">MySQL</div>
+  uiUxCollapsePressed = () => {
+    this.setState({
+      uiUxCollapse: !this.state.uiUxCollapse
+    });
+  };
+
+  render() {
+    return (
+      <div className="skill" id="skill">
+        <h1 className="text-center p-4">SKILLS</h1>
+
+        <div className="container pb-5">
+          <div className="card mb-3">
+            <div className="card-header">
+              <h3 onClick={this.developmentCollapsePressed}>Frameworks</h3>
+            </div>
+            {this.state.developmentCollapse === true ? (
+              <div className="card-body">
+                <blockquote className="blockquote mb-0">
+                  <div className="container">
+                    <div className="row">
+                      <div className="col">
+                        <img
+                          className="skillPic"
+                          src={BLOGO}
+                          alt="bootstrap logo"
+                        />
+                        <div className="col">BOOTSTRAP</div>
+                      </div>
+                      <div className="col">
+                        <img
+                          className="skillPic"
+                          src={RLogo}
+                          alt="react logo"
+                        />
+                        <div className="col">REACT</div>
+                      </div>
+                      <div className="col">
+                        <img
+                          className="skillPic"
+                          src={angular}
+                          alt="angular logo"
+                        />
+                        <div className="col">ANGULAR</div>
+                      </div>
+                      <div className="col">
+                        <img className="skillPic" src={node} alt="node logo" />
+                        <div className="col">Node.Js</div>
+                      </div>
+                    </div>
                   </div>
-                  <div className="col">
-                    <img
-                      className="skillPic"
-                      src={mongodb}
-                      alt="mongodb logo"
-                    />
-                    <div className="col">MongoDB</div>
-                  </div>
-                </div>
+                </blockquote>
               </div>
-            </blockquote>
+            ) : null}
+          </div>
+
+          <div className="card mb-3">
+            <div className="card-header">
+              <h3 onClick={this.uiUxCollapsePressed}>Languages</h3>
+            </div>
+            {this.state.uiUxCollapse === true ? (
+              <div className="card-body">
+                <blockquote className="blockquote mb-0">
+                  <div className="container">
+                    <div className="row">
+                      <div className="col">
+                        <img className="skillPic" src={java} alt="java logo" />
+                        <div className="col">JAVA</div>
+                      </div>
+                      <div className="col">
+                        <img
+                          className="skillPic"
+                          src={javascript}
+                          alt="javascript logo"
+                        />
+                        <div className="col">JAVASCRIPT</div>
+                      </div>
+                      <div className="col">
+                        <img
+                          className="skillPic"
+                          src={python}
+                          alt="python logo"
+                        />
+                        <div className="col">PYTHON</div>
+                      </div>
+                      <div className="col">
+                        <img className="skillPic" src={cplus} alt="c++ logo" />
+                        <div className="col">C++</div>
+                      </div>
+                      <div className="col">
+                        <img
+                          className="skillPic"
+                          src={CSS}
+                          alt="Bootstrap logo"
+                        />
+                        <div className="col">CSS</div>
+                      </div>
+                      <div className="col">
+                        <img
+                          className="skillPic"
+                          src={HTML5}
+                          alt="HTML5 logo"
+                        />
+                        <div className="col">HTML5</div>
+                      </div>
+                    </div>
+                  </div>
+                </blockquote>
+              </div>
+            ) : null}
+          </div>
+
+          <div className="card">
+            <div className="card-header">
+              <h3 onClick={this.peopleSkillCollapsePressed}>Backend</h3>
+            </div>
+            {this.state.peopleSkillCollapse === true ? (
+              <div className="card-body">
+                <blockquote className="blockquote mb-0">
+                  <div className="container">
+                    <div className="row">
+                      <div className="col">
+                        <img
+                          className="skillPic"
+                          src={mysql}
+                          alt="mysql logo"
+                        />
+                        <div className="col">MySQL</div>
+                      </div>
+                      <div className="col">
+                        <img
+                          className="skillPic"
+                          src={mongodb}
+                          alt="mongodb logo"
+                        />
+                        <div className="col">MongoDB</div>
+                      </div>
+                    </div>
+                  </div>
+                </blockquote>
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Skill;
