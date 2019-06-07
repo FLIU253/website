9dbee936babf18cb3732093ca7d9d1fa744da838
Franklin Liu <Franklin.Liu@autodatacorp.org>
2019-06-07 09:56:28 -0400

    navbar functionalities

--- src/App.js
+++ src/App.js
@@ -10,8 +10,23 @@ import Interest from "./Interest";
 import AOS from "aos";
 import "../node_modules/aos/dist/aos.css";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import IntersectionVisible    from 'react-intersection-visible';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      profileActive: false,
+      aboutActive: false,
+      experienceActive: false,
+      projectActive: false,
+      skillActive: false,
+      articleActive: false,
+      contactActive: false
+
+    }
+  }
+
   componentDidMount() {
     AOS.init();
   }
@@ -19,49 +34,83 @@ class App extends Component {
     AOS.refresh();
   }
 
+  setColor = (location) => {
+    if(location === "profile")
+    {
+      this.setState({profileActive: true});
+    }else if(location === "about"){
+      this.setState({aboutActive: true});
+    }else if(location === "experience"){
+      this.setState({experienceActive: true})
+    }else if(location === "project"){
+      this.setState({projectActive: true})
+    }else if(location === "skill"){
+      this.setState({skillActive: true})
+    }else if(location === "article"){
+      this.setState({articleActive: true})
+    }else if(location === "contact"){
+      this.setState({contactActive: true})
+    }
+  }
+  removeColor = (location) => { 
+    if(location === "profile")
+    {
+      this.setState({profileActive: false});
+    }else if(location === "about"){
+      this.setState({aboutActive: false});
+    }else if(location === "experience"){
+      this.setState({experienceActive: false})
+    }else if(location === "project"){
+      this.setState({projectActive: false})
+    }else if(location === "skill"){
+      this.setState({skillActive: false})
+    }else if(location === "article"){
+      this.setState({articleActive: false})
+    }else if(location === "contact"){
+      this.setState({contactActive: false})
+    }
+  }
   onUpArrowClick = () => {
     this.refs.landing.scrollIntoView({ behavior: "smooth" });
     console.log("test");
   };
-  onDownArrowClick = () => {
-    console.log("test");
-  };
+
   render() {
     return (
       <div className="all-content">
         <div className="sidebar">
           <ul>
-            <li className="list-section">
+            <li className= {this.state.profileActive ? "list-section profile-tag" : "list-section"}>
               <AnchorLink offset={() => 100} href="#landing">
                 Profile
               </AnchorLink>
             </li>
-            <li className="list-section">
+            <li className={this.state.aboutActive ? "list-section about-tag" : "list-section"}>
               <AnchorLink offset={() => 100} href="#about">
                 About
               </AnchorLink>
             </li>
-            <li className="list-section">
+            <li className={this.state.experienceActive ? "list-section experience-tag" : "list-section"}>
               <AnchorLink offset={() => 100} href="#experience">
                 Experience
               </AnchorLink>
             </li>
-            <li className="list-section">
+            <li className={this.state.projectActive ? "list-section project-tag" : "list-section"}>
               <AnchorLink offset={() => 100} href="#project">
                 Projects
               </AnchorLink>
             </li>
-            <li className="list-section">
+            <li className={this.state.skillActive ? "list-section skill-tag" : "list-section"}>
               <AnchorLink offset={() => 100} href="#skill">
                 Skills
               </AnchorLink>
             </li>
-            <li className="list-section">
+            <li className={this.state.articleActive ? "list-section article-tag" : "list-section"}>
               <AnchorLink offset={() => 100} href="#article">
                 Articles
               </AnchorLink>
             </li>
-            <li className="list-section">
+            <li className={this.state.contactActive ? "list-section contact-tag" : "list-section"}>
               <AnchorLink offset={() => 100} href="#contact">
                 Contacts
               </AnchorLink>
@@ -74,15 +123,50 @@ class App extends Component {
           style={{ fontSize: "1.5em" }}
           onClick={this.onUpArrowClick}
         />
+        <IntersectionVisible
+        onIntersect={(entries) => console.log(entries)}
+        onShow={() => this.setColor("profile")}
+        onHide={() => this.removeColor("profile")}>
         <div ref="landing">
           <Landing />
         </div>
+        </IntersectionVisible>
+        <IntersectionVisible
+        onIntersect={(entries) => console.log(entries)}
+        onShow={() => this.setColor("about")}
+        onHide={() => this.removeColor("about")}>
         <About />
+        </IntersectionVisible>
+        <IntersectionVisible
+        onIntersect={(entries) => console.log(entries)}
+        onShow={() => this.setColor("experience")}
+        onHide={() => this.removeColor("experience")}>
         <Experience />
+        </IntersectionVisible>
+        <IntersectionVisible
+        onIntersect={(entries) => console.log(entries)}
+        onShow={() => this.setColor("project")}
+        onHide={() => this.removeColor("project")}>
         <Project />
+        </IntersectionVisible>
+        <IntersectionVisible
+        onIntersect={(entries) => console.log(entries)}
+        onShow={() => this.setColor("skill")}
+        onHide={() => this.removeColor("skill")}>
         <Skill />
+        </IntersectionVisible>
+        <IntersectionVisible
+        onIntersect={(entries) => console.log(entries)}
+        onShow={() => this.setColor("article")}
+        onHide={() => this.removeColor("article")}>
         <Interest />
+        </IntersectionVisible>
+        <IntersectionVisible
+        onIntersect={(entries) => console.log(entries)}
+        onShow={() => this.setColor("contact")}
+        onHide={() => this.removeColor("contact")}>
         <Footer />
+        </IntersectionVisible>
       </div>
     );
   }
