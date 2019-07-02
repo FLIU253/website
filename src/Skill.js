855478938aba411bae3ecd577b31d611affdea63
Franklin Liu <Franklin.Liu@autodatacorp.org>
2019-07-02 15:56:27 -0400

    removed logos for skills

--- src/Skill.js
+++ src/Skill.js
@@ -1,19 +1,6 @@
 import React, { Component } from "react";
 import "./Skill.css";
-import CSS from "./images/CSS.png";
-import HTML5 from "./images/HTML5.png";
-import BLOGO from "./images/bootstrap.png";
-import RLogo from "./images/React.png";
-import angular from "./images/angular.png";
-import node from "./images/node.png";
-import mysql from "./images/mysql.png";
-import java from "./images/Java.png";
-import javascript from "./images/javascript.png";
-import python from "./images/python.png";
-import cplus from "./images/c++.png";
-import json from "./images/json.png";
-import xml from "./images/xml.png";
-import git from "./images/git.png";
+
 
 class Skill extends Component {
   constructor(props) {
@@ -85,64 +72,16 @@ class Skill extends Component {
                 >
                   <blockquote>
                     <div className="container">
-                      <div className="row">
-                        <div className="col">
-                          <img
-                            className="skillPic"
-                            src={javascript}
-                            alt="javascript logo"
-                          />
-                          <div className="col">JavaScript</div>
-                        </div>
-                        <div className="col">
-                          <img
-                            className="skillPic"
-                            src={java}
-                            alt="java logo"
-                          />
-                          <div className="col">Java</div>
-                        </div>
-                        <div className="col">
-                          <img
-                            className="skillPic"
-                            src={python}
-                            alt="python logo"
-                          />
-                          <div className="col">Python</div>
-                        </div>
-                        <div className="col">
-                          <img
-                            className="skillPic"
-                            src={cplus}
-                            alt="c++ logo"
-                          />
-                          <div className="col">C++</div>
-                        </div>
-                        <div className="col">
-                          <img
-                            className="skillPic"
-                            src={mysql}
-                            alt="SQL logo"
-                          />
-                          <div className="col">SQL</div>
-                        </div>
-                        <div className="col">
-                          <img
-                            className="skillPic"
-                            src={json}
-                            alt="json logo"
-                          />
-                          <div className="col">JSON</div>
-                        </div>
-                        <div className="col">
-                          <img className="skillPic" src={xml} alt="xml logo" />
-                          <div className="col">XML</div>
-                        </div>
-                        <div className="col">
-                          <img className="skillPic" src={git} alt="git logo" />
-                          <div className="col">Git</div>
-                        </div>
-                      </div>
+                    <div className="row text-left">
+                      <ul>
+                        <li>JavaScript</li>
+                        <li>Java</li>
+                        <li>C++</li>
+                        <li>Python</li>
+                        <li>mySQL</li>
+                        <li>MongoDB</li>
+                      </ul>
+                    </div>
                     </div>
                   </blockquote>
                 </div>
@@ -173,56 +112,16 @@ class Skill extends Component {
                 >
                   <blockquote>
                     <div className="container">
-                      <div className="row">
-                        <div className="col">
-                          <img
-                            className="skillPic"
-                            src={BLOGO}
-                            alt="bootstrap logo"
-                          />
-                          <div className="col">Bootstrap</div>
-                        </div>
-                        <div className="col">
-                          <img
-                            className="skillPic"
-                            src={RLogo}
-                            alt="react logo"
-                          />
-                          <div className="col">React</div>
-                        </div>
-                        <div className="col">
-                          <img
-                            className="skillPic"
-                            src={angular}
-                            alt="angular logo"
-                          />
-                          <div className="col">Angular</div>
-                        </div>
-                        <div className="col">
-                          <img
-                            className="skillPic"
-                            src={node}
-                            alt="Nodejs logo"
-                          />
-                          <div className="col">Nodejs</div>
-                        </div>
-                        <div className="col">
-                          <img
-                            className="skillPic"
-                            src={CSS}
-                            alt="Bootstrap logo"
-                          />
-                          <div className="col">CSS</div>
-                        </div>
-                        <div className="col">
-                          <img
-                            className="skillPic"
-                            src={HTML5}
-                            alt="HTML5 logo"
-                          />
-                          <div className="col">HTML5</div>
-                        </div>
-                      </div>
+                    <div className="row text-left">
+                      <ul>
+                        <li>Reactjs</li>
+                        <li>Vuejs</li>
+                        <li>Angularjs</li>
+                        <li>Nodejs</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                      </ul>
+                    </div>
                     </div>
                   </blockquote>
                 </div>
@@ -293,16 +192,15 @@ class Skill extends Component {
                       : "card-body dropdown-not-visible"
                   }
                 >
-                  <blockquote className="blockquote mb-0">
                     <div className="container">
                       <div className="row text-left">
                         <ul>
                           <li>Quality Assurance</li>
                           <li>Software Testing</li>
+                          <li>Git Version Control</li>
                         </ul>
                       </div>
                     </div>
-                  </blockquote>
                 </div>
               ) : null}
             </div>
